c175761b74103c9ca623183ba4eed79150085ba8
AbdulMoiz-Ali <abdulmoizali321@gmail.com>
2024-07-02 01:16:50 +0500

    Function-JavaScript

---
+++ app.js
@@ -0,0 +1,217 @@
+document.write("<br>");
+document.write("<h3>Question no 01</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques1() ">Click Me</button>');
+document.write("<br>");
+document.write("<br>");
+function ques1() {
+    var output = 'Sat April 15 2024 22:23:21 GMT+0500 (PKR)'
+    document.getElementById("out").innerHTML = output;
+}
+document.write(`<h4 id="out"></h4><br>`)
+// question no 2
+
+document.write("<h3>Question no 02</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques2();">Click Me</button>');
+function ques2() {
+    var use1 = prompt('Please! Put Your FIrst Name.')
+    var use2 = prompt('Please! Put Your Second Name.')
+    alert(use1 + ' ' + use2)
+    document.getElementById("outn").innerHTML = use1 + ' ' + use2;
+}
+document.write(`<br><br><h4 id="outn"></h4>`)
+document.write("<br>");
+document.write("<br>");
+
+//question no 3
+
+document.write("<h3>Question no 03</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques3()">Click Me</button>');
+function ques3() {
+    var num1 = parseInt(prompt('Please! Put Your FIrst Number.'))
+    var num2 = parseInt(prompt('Please! Put Your Second Nunber.'))
+    alert(num1 + num2)
+    document.getElementById("outu").innerHTML = num1 + num2;
+}
+document.write(`<br><br><h4 id="outu"></h4>`)
+document.write("<br>");
+document.write("<br>");
+
+//question no 4
+
+document.write("<h3>Question no 04</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques4()">Click Me</button>');
+function ques4() {
+    var firstnum = parseInt(prompt("Enter a first number:"));
+    var secondnum = parseInt(prompt("Enter a second number:"));
+    var thirednum = prompt("Enter a Operation (+, -, *, /, %):");
+    if (thirednum === "+") {
+        console.log(firstnum + secondnum);
+        alert(firstnum + secondnum);
+    } else if (thirednum === "-") {
+        console.log(firstnum - secondnum);
+        alert(firstnum - secondnum);
+    } else if (thirednum === "/") {
+        console.log(firstnum / secondnum);
+        alert(firstnum / secondnum);
+    } else if (thirednum === "*") {
+        console.log(firstnum * secondnum);
+        alert(firstnum * secondnum);
+    } else if (thirednum === "%") {
+        console.log(firstnum % secondnum);
+        alert(firstnum % secondnum);
+    } else {
+        console.log(thirednum + " is not  a define.");
+        alert(thirednum + " is not a define.");
+    }
+};
+document.write("<br>");
+document.write("<br>");
+//question no 5
+document.write("<h3>Question no 05</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques5()">Click Me</button>');
+function ques5() {
+
+}
+
+// document.write('<div id="pere"></div>')
+document.write("<br>");
+document.write("<br>");
+
+//question no 6
+
+document.write("<h3>Question no 06</h3>");
+document.write("<br>");
+document.write("<h3>Generator MarkSheet</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques6()"">Click Me</button>');
+function ques6() {
+
+}
+// document.write('<div id="result"></div>')
+document.write("<br>");
+document.write("<br>");
+
+
+//question no 7
+
+document.write("<h3>Question no 07</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques7()">Click Me</button>');
+function ques7() {
+
+}
+document.write("<br>");
+document.write("<br>");
+
+
+//question no 8
+
+document.write("<h3>Question no 08</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques8()">Click Me</button>');
+function ques8() {
+
+}
+document.write("<br>");
+document.write("<br>");
+
+//question no 9
+
+document.write("<h3>Question no 09</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques9()">Click Me</button>');
+function ques9() {
+
+}
+document.write("<br>");
+document.write("<br>");
+
+//question no 10
+
+document.write("<h3>Question no 10</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques10()">Click Me</button>');
+function ques10() {
+
+}
+document.write("<br>");
+document.write("<br>");
+
+//question no 11
+
+document.write("<h3>Question no 11</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques11()">Click Me</button>');
+function ques11() {
+
+}
+document.write("<br>");
+document.write("<br>");
+
+//question no 12
+
+document.write("<h3>Question no 12</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques12()">Click Me</button>');
+function ques12() {
+
+}
+document.write("<br>");
+document.write("<br>");
+
+//question no 13
+
+document.write("<h3>Question no 13</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques13()">Click Me</button>');
+function ques13() {
+
+}
+document.write("<br>");
+document.write("<br>");
+
+//question no 14
+
+document.write("<h3>Question no 14</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques14()">Click Me</button>');
+function ques14() {
+
+}
+document.write("<br>");
+document.write("<br>");
+
+document.write('<div style="color: blue;background-color: black;"><br><h1>Best OF luck<span style="font-size:50px;">👋</span></h1><br></div>')//&#128075
